808e9fdc35615f2fab071e6cda5968d3e1739cfb
Piseth Ing <piseth.ing@allianz.com>
2023-10-18 16:28:12 +0700

    day 22 update docker compose using health check

--- account/src/main/java/com/piseth/bank/account/AccountApplication.java
+++ account/src/main/java/com/piseth/bank/account/AccountApplication.java
@@ -3,11 +3,11 @@ package com.piseth.bank.account;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+//import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @RefreshScope
 @SpringBootApplication
-@EnableFeignClients
+//@EnableFeignClients
 public class AccountApplication {
 
 	public static void main(String[] args) {
--- account/src/main/java/com/piseth/bank/account/service/client/LoanFeignClient.java
+++ account/src/main/java/com/piseth/bank/account/service/client/LoanFeignClient.java
@@ -2,14 +2,14 @@ package com.piseth.bank.account.service.client;
 
 import java.util.List;
 
-import org.springframework.cloud.openfeign.FeignClient;
+//import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 import com.piseth.bank.account.dto.LoanResponseDTO;
 
-@FeignClient(name = "loan")
+//@FeignClient(name = "loan")
 public interface LoanFeignClient {
 	@GetMapping("/api/loans/{customerId}")
 	List<LoanResponseDTO> getLoanInfo(
